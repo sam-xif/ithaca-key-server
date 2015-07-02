9f223444f38d8561407b03bdf2e913e721543d85
sam-xif <s.xifaras999@gmail.com>
2015-07-01 20:54:03 -0400

    Fixed using statements

--- Program.cs
+++ Program.cs
@@ -2,15 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 using System.IO;
 
-using System.Xml.Linq;
+using System.Xml;
 
 using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Net.Security;
 
 using System.Web;
